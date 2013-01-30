a317098fea446ab1f0bd976fed800a137a0a1446
RustyRadio <danielc@my.ccsu.edu>
2013-01-30 09:25:38 -0800

    Create counter.java

---
+++ counter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class counter {
+  
+  public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double[] values= {0.25,0.1,0.05,0.01};
+		String[] coin= {"Quarter:", "Dime", "Nickel", "Penny"};
+		double[] time= {0,0,0,0};
+		double input = in.nextDouble();
+		input= Math.round(input * 100);
+		input= input/100;
+		double temp = input;
+		for (int i = 0; i < 4; i++) {
+		    	while (temp >= values[i]) {
+		    		temp = Math.round(temp * 100);
+		    		temp = temp/100;
+		    		time[i]++;
+		    		temp -= values[i];
+		    	}
+		    }
+		if (temp > 0 && temp < 0.01) time[3]++;
+		for (int i = 0; i < 4; i++) {
+			if (time[i] == 0) {} else {
+			System.out.println(coin[i] + " " + time[i]);
+			}
+		}
+	}
+}
